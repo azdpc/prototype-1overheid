ea6352e1c6167883175961e3015e610efa674857
Pim Dubois <pim.dubois@capgemini.com>
2019-10-28 11:36:21 +0100

    basic settings for including html added

--- js/main.js
+++ js/main.js
@@ -381,4 +381,29 @@ $.when($.ready).then(function() {
     setIframeStatus();
     handleHelpTexts();
     toggleButton();
+    includeHTML();
 });
+
+// Include HTML
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("azdpc-include-html");
+        if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            elmnt.removeAttribute("azdpc-include-html");
+            includeHTML();
+            }
+        }      
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+        }
+    }
+};
